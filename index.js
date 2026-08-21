4ba394c5723f1c132038e218952a4ffddd0ff7a4
Jorge Chocobar <jorge1986e@gmail.com>
2026-08-21 17:37:16 -0300

    creando ficha de juego

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const juego = {
-    titulo: 'Marvel Tokon',
+    titulo: "Marvel Tokon",
     estudio: "Arc System Works",
     distribuidor: "Sony Interactive Entertainment",
-    clasifiCacion: "T teen",
+    clasificacion: "T teen",
     plataformas: ["PlayStation 5", "Windows"],
-    multijugador: 'true',
+    multijugador: "true",
     idioma: ["japones", "ingles"],
     anio: 2026,
 }
+//------------------Ficha de Juego------------------//
+
+const nombre = process.argv[2] ?? "Estudiante";
+console.log(`Bienvenido: ${nombre}`);
+
+const node = process.version;
+const os = process.platform;
+
+const plataformasComoTexto = juego.plataformas.join(", ");
+const idiomaComoTexto = juego.idioma.join(", ");
+const multijugador = juego.multijugador ? "Si" : "No";
+
+const fichaDeJuego = `FICHA DE JUEGO
+
+Estudiante: ${nombre}
+Node js: ${node}
+Plataforma del sistema: ${os}
+
+Nombre de Juego: ${juego.titulo}
+Desarrolador: ${juego.estudio}
+Distribuidor: ${juego.distribuidor}
+Clasificacion ${juego.clasificacion}
+Plataformas: ${plataformasComoTexto}
+Multijugador: ${multijugador}
+idioma: ${idiomaComoTexto}
+Año: ${juego.anio}`;
+
+
