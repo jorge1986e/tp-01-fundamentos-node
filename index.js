0beb6bc984ba6536e2fb60de5423a7afbe8facf1
Jorge Chocobar <jorge1986e@gmail.com>
2026-08-21 16:40:02 -0300

    inicio: creado objeto de tp

---
+++ index.js
@@ -0,0 +1,10 @@
+const juego = {
+    titulo: 'Marvel Tokon',
+    estudio: "Arc System Works",
+    distribuidor: "Sony Interactive Entertainment",
+    clasifiCacion: "T teen",
+    plataformas: ["PlayStation 5", "Windows"],
+    multijugador: 'true',
+    idioma: ["japones", "ingles"],
+    anio: 2026,
+}
