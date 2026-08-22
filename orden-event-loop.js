7f736cc7af63d539c12e804d8c36f96c2ea3c4b5
Jorge Chocobar <jorge1986e@gmail.com>
2026-08-22 15:57:13 -0300

    event-loop

--- orden-event-loop.js
+++ orden-event-loop.js
@@ -1,3 +1,6 @@
-console.log();
-console.log();
-console.log();
+console.log("Comienza el programa");
+
+setTimeout(()=>{
+console.log("Se ejecuta la tarea programada");}, 0)
+
+console.log("Termina el código principal");
