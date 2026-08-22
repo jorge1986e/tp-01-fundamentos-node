ae5ed1b9ea207be08c88f5470eb9e48e24f86798
Jorge Chocobar <jorge1986e@gmail.com>
2026-08-22 15:31:42 -0300

    Creación del archivo: path.join y fs

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const fs = require("node:fs");
+const path = require("node:path");
+
+
+
 const juego = {
     titulo: "Marvel Tokon",
+    genero: "pelea",
     estudio: "Arc System Works",
     distribuidor: "Sony Interactive Entertainment",
     clasificacion: "T teen",
@@ -20,19 +26,31 @@ const plataformasComoTexto = juego.plataformas.join(", ");
 const idiomaComoTexto = juego.idioma.join(", ");
 const multijugador = juego.multijugador ? "Si" : "No";
 
-const fichaDeJuego = `FICHA DE JUEGO
+const fichaDeJuego = `FICHA DEL JUEGO
 
 Estudiante: ${nombre}
 Node js: ${node}
 Plataforma del sistema: ${os}
 
-Nombre de Juego: ${juego.titulo}
-Desarrolador: ${juego.estudio}
+Titulo: ${juego.titulo}
+Genero: ${juego.genero}
+Estudio: ${juego.estudio}
 Distribuidor: ${juego.distribuidor}
 Clasificacion ${juego.clasificacion}
 Plataformas: ${plataformasComoTexto}
-Multijugador: ${multijugador}
+¿Es multijugador?: ${multijugador}
 idioma: ${idiomaComoTexto}
 Año: ${juego.anio}`;
+
+//------------------Creación del archivo------------------//
+
+const carpetaDeSalida = path.join(__dirname, "Salida");
+const rutaFicha = path.join(carpetaDeSalida, "ficha-videojuego.txt");
+
+fs.mkdirSync(carpetaDeSalida, {recursive: true});
+fs.writeFileSync(rutaFicha, fichaDeJuego, "utf8");
+
+console.log(fichaDeJuego);
+console.log(`ruta del archivo generado: ${rutaFicha}`);
 
 
